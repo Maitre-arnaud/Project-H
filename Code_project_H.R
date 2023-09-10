ff3f5f515715543679c2710f6be0c0c78298946f
Maitre-arnaud <115786838+Maitre-arnaud@users.noreply.github.com>
2023-09-10 22:23:08 +0200

    simple OLS everywhere

--- Code_project_H.R
+++ Code_project_H.R
@@ -10,6 +10,7 @@ library(DescTools) # winsorize
 library(expss)
 library(readr)
 library(lme4)
+library(openxlsx)
 
 user <- 'Arnaud'
 if(user=='Arnaud'){
@@ -38,10 +39,11 @@ standardize <- function(x){
 }
 
 scale <- function(x){
-  mi <- min(x, na.rm=T)
-  ma <- max(x, na.rm=T)
-  y <- (x-mi)/(ma-mi)
-  return(y)
+  y <- as.numeric(x)
+  mi <- min(y, na.rm=T)
+  ma <- max(y, na.rm=T)
+  z <- (y-mi)/(ma-mi)
+  return(z)
 }
 
 put_stars <- function(coef, se, scale=1, decimal=2){
@@ -73,54 +75,50 @@ put_stars <- function(coef, se, scale=1, decimal=2){
 
 # Data Preparation --------------------------------------------------------
 # read and winsorize
-data <- read_sav(file.path(my_path, 'PANDA data_2021_11_17.sav'))
-#intra <- read_sav(file.path(my_path, '54-countries-6792-published-INTRA-data.sav'))
-data <- my_wins(data, 0.05, c('eu_inc_ppp', 'ppl_hh')) # some crazy values to change
-data <- my_wins(data, 0.01, c('age')) # less crazy values
-data <- data[data$cntry==data$nationality,]
-
-# wws <- readr::read_csv(file.path(my_path, 
-#                                  'WVS_Cross-National_Inverted_Wave_7_csv_v5_0.csv'))
+data <- read.xlsx(xlsxFile=file.path(
+  my_path, 'PANDA-Excel-version-with-documentation.xlsx'), sheet='data')
+data <- my_wins(data, 0.05, c('eu_ppp_income')) # some crazy values to change
+#data <- data[data$country_code==data$nationality,]
+data <- data[data$base_pack_quality>0,] # drop bad quality answers (NA stays)
+data <- data[data$tech_pack_quality>0,]
 
 # Attitude toward learning:
 #degree -> 1 to 5 scale (5=PhD or equivalent)
 # no clue what is the difference with hdegree but the last one has less nan values
 # XB2 -> I see myself as trusting
 #ALA4 -> 1 to 5 scale (5=Agree with Somebody who studies hard should be admired)
-#ALC2 -> 1 to 5 scale (5=wants to be firend with diligent student)
+#ALC2 -> 1 to 5 scale (5=wants to be friend with diligent student)
 # unigrade -> 1 to 100 (100=best grade)
 # patience (MS X.T1) -> 1 = a payment of 1700 this month / 2 = a payment of 1900 next month 
-# eu_inc_ppp = monthly income adjusted for difference in purchasing power (CONTROL)
-# ppl_hh = n_pers in household (CONTROL)
+# eu_ppp_income = monthly income adjusted for difference in purchasing power
 att_learning <- c('ALA1', 'ALA2', 'ALA3', 'ALA4', 'ALB1', 'ALB2', 'ALC2', 
                   'ALC3', 'ALC5', 'ALC6', 'ALD1', 'ALD4')
 # reverse them for highest score = good attitude toward learning
 att_learning_to_inv <- c('ALA2', 'ALA3', 'ALC5', 'ALC6')
-hof <- c('IDV', 'PDI1', 'MAS', 'UAI') # 'PDI2'
+hof <- c('IDV', 'PDI', 'MAS', 'UAI')
+
+# some columns names are duplicated drop them -> mess with dplyr
+dupl_cols <- colnames(data)[which(duplicated(names(data)))]
+data <- data[ , -which(names(data) %in% dupl_cols)]
 
 # Data preparation
 data <- data %>% 
-  mutate(wealth_hh_per_ppl = log(eu_inc_ppp/ppl_hh),
-         date = lubridate::dmy(substr(datetime, start=1, stop=11)),
-         year = lubridate::year(date),
-         is_patient = case_when(patience==1 ~ 0, T ~ 1),
-         age = log(age),
+  mutate(wealth = log(eu_ppp_income),
+         family_size = 1 + as.numeric(married) + as.numeric(children),
+         is_patient = case_when(XT1==2 ~ 1, T ~ 0), # 2 = choose to wait
+         age = log(as.numeric(age)),
          trust = scale(XB2),
-         female = case_when(gender==0 ~ 1, T ~ 0),
-         across(all_of(att_learning_to_inv), ~ .x*-1, .names = '{.col}'),
+         across(all_of(att_learning_to_inv), ~ as.numeric(.x)*-1, .names = '{.col}'),
          across(all_of(att_learning), ~ scale(.x), .names = '{.col}'),
          across(all_of(hof), ~ scale(.x), .names = '{.col}')) %>% 
-  select(is_patient, wealth_hh_per_ppl, date, cntry, 
-         all_of(att_learning), hdegree, age, year, trust, female, 
-         all_of(hof), EU_cntry, Asia_cntry)
+  select(is_patient, family_size, country_code, wealth,
+         all_of(att_learning), uni_degree, age, trust, female, 
+         all_of(hof), eu_country, Asia, nationality)
 
 # create a score attitude toward learning
 data$n_var_atl <- apply(!is.na(data[, att_learning]), 1, sum) # n non-missing var
 data <- data[data$n_var_atl>0,]
 data$atl <-  rowSums(data[,att_learning], na.rm = T) / data$n_var_atl
-
-# plot data points
-plot(data$is_patient, data$atl)
 
 #######################################################################
 ## TEST 1: Link between highest diploma obtained and patience
@@ -128,24 +126,36 @@ plot(data$is_patient, data$atl)
 
 # drop nan
 test1 <- data
-test1 <- na.omit(test1[, c('is_patient', 'wealth_hh_per_ppl', 'date', 
-                              'cntry', 'hdegree', 'age', 'trust', 
-                           'female', 'atl')])
+colSums(is.na(test1))
+test1 <- na.omit(test1[, c('is_patient', 'wealth', 'country_code', 'uni_degree', 
+                           'age', 'trust', 'female', 'atl', 'family_size')])
+xs <- colnames(test1 %>% select(-country_code, -atl))
 
 #regression (panel regression)
-formulaff_1 <- as.formula(
-  'is_patient ~ atl + hdegree + wealth_hh_per_ppl + age + trust + female |cntry| 0 |cntry',
-  )
+formulaff_1 <- as.formula(paste(
+  'atl ~ ', paste(xs, collapse = '+'), '|country_code| 0 |country_code',
+  ), sep='')
 regff_1 <- felm(formulaff_1, data=test1, exactDOF = T, cmethod='reghdfe')
 summary(regff_1)
 
-# output in nice table
-res <- summary(regff_1)$coefficients
+#regression multilevel
+# with random intercept for each group (=country_code)
+formulaml_1 <- as.formula(paste(
+  'atl ~ ', paste(xs, collapse = '+'), ' + (1 |country_code)', sep=''))
+regml_1 <- lmer(formulaml_1, data = test1)
+summary(regml_1)
+
+#regression basic ols
+formula_1 <- as.formula(paste(
+  'atl ~ 1 +', paste(xs, collapse = '+'), sep=''))
+reg_1 <- lm(formula_1, data = test1)
+summary(reg_1)
 
 # results -> nice table
+res <- summary(reg_1)$coefficients
 coef_str <- c()
-clean_var <- c('ATL_{i}', 'Degree_{i}', 'Wealth_{i}', 'Age_{i}', 'Trust_{i}', 
-               'IsFemale_{i}')
+clean_var <- c('Intercept', 'IsPatient_{i}', 'Wealth_{i}', 'HasUniDegree_{i}', 
+               'Age_{i}', 'Trust_{i}', 'IsFemale_{i}', 'FamilySize_{i}')
 
 for(i in seq(1, length(clean_var))){
   coef_str[i] <- paste('\\multirow{2}{*}{$', clean_var[i], '$} & ', 
@@ -154,22 +164,6 @@ for(i in seq(1, length(clean_var))){
                        ')$\\\\[3pt]\n', sep='')
 }
 cat(coef_str)
-
-
-#regression multilevel
-# with random intercept for each group (=cntry)
-xs <- c('atl', 'hdegree', 'wealth_hh_per_ppl', 'age', 'trust', 'female')
-formulaml_1 <- as.formula(paste(
-  'is_patient ~ ', paste(xs, collapse = '+'), ' + (1 |cntry)', sep=''))
-regml_1 <- lmer(formulaml_1, data = test1)
-summary(regml_1)
-
-#regression basic ols
-xs <- c('atl', 'hdegree', 'wealth_hh_per_ppl', 'age', 'trust', 'female')
-formula_1 <- as.formula(paste(
-  'is_patient ~ 1+', paste(xs, collapse = '+'), sep=''))
-reg_1 <- lm(formula_1, data = test1)
-summary(reg_1)
 
 # We see in summary(regff_1), that atl is positively linked to patience
 # the effect is robust to country fixed effects which include time-invariants country
@@ -180,15 +174,15 @@ summary(reg_1)
 #######################################################################
 
 # barplot
-test2_plot <- data %>% group_by(cntry) %>% 
+test2_plot <- data %>% group_by(country_code) %>% 
   mutate(
     mean_is_patient = mean(is_patient, na.rm=T),
     mean_atl = mean(atl, na.rm=T)) %>% ungroup() %>% 
-  select(cntry, mean_atl, mean_is_patient) %>% unique() %>% 
-  arrange(cntry)
-barplot(cbind(mean_is_patient, mean_atl) ~ cntry, data=test2_plot, 
-        beside=T, names.arg=c('Germany', 'Estonia', 'Taiwan', 'China', 
-                              'Vietnam', 'Japan', 'Hong Kong'),
+  select(country_code, mean_atl, mean_is_patient) %>% unique() %>% 
+  arrange(country_code)
+barplot(cbind(mean_is_patient, mean_atl) ~ country_code, data=test2_plot, 
+        beside=T, names.arg=c('Estonia', 'Taiwan', 'China', 'Vietnam', 
+                              'Germany', 'Hong Kong', 'Japan'),
         xlab = '', ylab = 'Proportion', col=c('dark blue', 'light blue'), 
         ylim=c(0,1))
 legend('top', col=c('dark blue', 'light blue'), 
@@ -203,35 +197,44 @@ scatter.smooth(test2_plot$mean_atl, test2_plot$mean_is_patient, span = 1,
 # drop nan
 test2 <- data
 colSums(is.na(test2))
-test2 <- na.omit(test2[c('is_patient', 'wealth_hh_per_ppl', 'date', 'cntry', 
-                         'hdegree', 'age', 'female', 'trust', 'atl')])
+test2 <- na.omit(test2[, c('is_patient', 'wealth', 'country_code', 'uni_degree', 
+                           'age', 'trust', 'female', 'atl', 'family_size')])
+xs <- colnames(test1 %>% select(-country_code, -atl))
 
 # run regression for each country
-countries <- sort(as.numeric(unique(test2$cntry)))
-formula_2 <- as.formula(
-  'is_patient ~ 1 + atl + hdegree + wealth_hh_per_ppl + age + trust + female')
+countries <- sort(as.numeric(unique(test2$country_code)))
+formula_2 <- as.formula(paste(
+  'atl ~ 1 + ', paste(xs, collapse = '+'), sep=''))
 results_2 <- list()
+n_obs <- c()
 
 for(i in seq(1, length(countries))){
   ci <- countries[[i]]
-  sub <- test2[test2$cntry==ci,]
+  sub <- test2[test2$country_code==ci,]
   reg_2 <- lm(formula_2, data=sub)
   coef_2 <- summary(reg_2)$coefficients
   results_2[[i]] <- coef_2
+  n_obs[i] <- dim(sub)[1]
 }
 
 # results -> nice table
 coef_str <- c()
-clean_var <- c('ATL_{i}', 'Degree_{i}', 'Wealth_{i}', 'Age_{i}', 'Trust_{i}', 
-               'IsFemale_{i}')
+clean_var <- c('Intercept', 'IsPatient_{i}', 'Wealth_{i}', 'HasUniDegree_{i}', 
+               'Age_{i}', 'Trust_{i}', 'IsFemale_{i}', 'FamilySize_{i}')
 
 for(i in seq(1, length(clean_var))){
-  coef_str[i] <- paste('\\multirow{2}{*}{$', clean_var[i], '$} & ', 
-                       put_stars(res[i,1], res[i,2], m, d), 
-                       '\\\\ \n & $(', format(round(res[i,3],d), scientific=F) ,
-                       ')$\\\\[3pt]\n', sep='')
+  ci <- paste('\\multirow{2}{*}{$', clean_var[i], '$}', sep='')
+  si <- ''
+  for(mi in seq(1, length(countries))){
+    reg <- results_2[[mi]]
+    ci <- paste(ci, ' & ', put_stars(reg[i,1], reg[i,2], m, d), sep='')
+    si <- paste(si, ' & $(' , format(round(reg[i,3],d), scientific=F), ')$', sep='')
+
+  }
+  coef_str[i] <- paste(ci, '\\\\ \n ', si, '\\\\[3pt]\n', sep='')
 }
 cat(coef_str)
+cat(paste(paste0('$', n_obs, '$'), collapse = ' & '))
 
 # Arnaud: Within countries, atl is not linked to patience
 # Differences in patience seems to be linked with country cultural difference
@@ -242,30 +245,30 @@ cat(coef_str)
 #######################################################################
 
 # drop nan
-test3 <- data
-colSums(is.na(test3))
-test3 <- na.omit(test3[, c('is_patient', 'wealth_hh_per_ppl', 'date', 
-                           'cntry', 'hdegree', 'age', 'trust', 
-                           'female', 'atl')])
-
-# create interactions terms within some countries
-test3 <- test3 %>% mutate(
-  atl_estonia = atl * case_when(cntry == 2 ~ 1, T ~ 0),
-  atl_taiwan = atl * case_when(cntry == 3 ~ 1, T ~ 0),
-  atl_china = atl * case_when(cntry == 4 ~ 1, T ~ 0),
-  atl_vietnam = atl * case_when(cntry == 5 ~ 1, T ~ 0),
-  atl_japan = atl * case_when(cntry == 6 ~ 1, T ~ 0))
-  #atl_hk = atl * case_when(cntry == 7 ~ 1, T ~ 0))
-expla_var <- c('atl', 'atl_estonia', 'atl_taiwan', 'atl_china', 'atl_vietnam', 
-               'atl_japan', 'hdegree', 'wealth_hh_per_ppl', 'age', 'trust', 
-               'female')
-
-# regression
-# cant use fixed effects as degree_xxx is fixed for some countries
-formula_3 <- as.formula(paste('is_patient ~ 1 +', paste(expla_var, collapse='+'), 
-                              sep=''))
-reg_3 <- lm(formula_3, data=test3)
-summary(reg_3)
+# test3 <- data
+# colSums(is.na(test3))
+# test3 <- na.omit(test3[, c('is_patient', 'wealth_hh_per_ppl', 'date', 
+#                            'cntry', 'hdegree', 'age', 'trust', 
+#                            'female', 'atl')])
+# 
+# # create interactions terms within some countries
+# test3 <- test3 %>% mutate(
+#   atl_estonia = atl * case_when(cntry == 2 ~ 1, T ~ 0),
+#   atl_taiwan = atl * case_when(cntry == 3 ~ 1, T ~ 0),
+#   atl_china = atl * case_when(cntry == 4 ~ 1, T ~ 0),
+#   atl_vietnam = atl * case_when(cntry == 5 ~ 1, T ~ 0),
+#   atl_japan = atl * case_when(cntry == 6 ~ 1, T ~ 0))
+#   #atl_hk = atl * case_when(cntry == 7 ~ 1, T ~ 0))
+# expla_var <- c('atl', 'atl_estonia', 'atl_taiwan', 'atl_china', 'atl_vietnam', 
+#                'atl_japan', 'hdegree', 'wealth_hh_per_ppl', 'age', 'trust', 
+#                'female')
+# 
+# # regression
+# # cant use fixed effects as degree_xxx is fixed for some countries
+# formula_3 <- as.formula(paste('is_patient ~ 1 +', paste(expla_var, collapse='+'), 
+#                               sep=''))
+# reg_3 <- lm(formula_3, data=test3)
+# summary(reg_3)
 
 #######################################################################
 ## TEST 4: Is the link between education and patience driven by cultural differences?
@@ -277,26 +280,32 @@ summary(reg_3)
 #   ungroup()
 
 # drop nan
-test4 <- data# %>% filter(cntry!=7) %>% filter(cntry!=2)
+test4 <- data
 colSums(is.na(test4))
-test4 <- na.omit(test4[, c('is_patient', 'wealth_hh_per_ppl', 'date', 
-                          'cntry', 'age', 'trust', 
-                          'female', 'atl', hof)])
-
-expla_var <- c('atl', 'wealth_hh_per_ppl', 'age', 'trust', 'female', hof)
+test4 <- na.omit(test4[, c('is_patient', 'wealth', 'country_code', 'uni_degree', 
+                           'age', 'trust', 'female', 'atl', 'family_size', 
+                           'Asia', hof)])
+xs <- colnames(test4 %>% select(-country_code, -atl))
 
 # regression
-# formula_4 <- as.formula(paste('is_patient ~', paste(expla_var, collapse='+'), 
-#                               sep=''))
-# reg_4 <- lm(formula_4, data=test4)
-# summary(reg_4)
-
-formula_4 <- as.formula(paste('is_patient ~', paste(expla_var, collapse='+'),
-                              '| cntry | 0 | 0', sep=''))
-reg_4 <- felm(formula_4, data=test4, exactDOF = T, cmethod='reghdfe')
+formula_4 <- as.formula(paste('atl ~', paste(xs, collapse='+'), sep=''))
+reg_4 <- lm(formula_4, data=test4)
 summary(reg_4)
 
+# results -> nice table
+res <- summary(reg_4)$coefficients
+coef_str <- c()
+clean_var <- c('Intercept', 'IsPatient_{i}', 'Wealth_{i}', 'HasUniDegree_{i}', 
+               'Age_{i}', 'Trust_{i}', 'IsFemale_{i}', 'FamilySize_{i}', 
+               'InAsia_{i}', 'IDV_{i}', 'PDI_{i}', 'MAS_{i}', 'UAI_{i}')
 
+for(i in seq(1, length(clean_var))){
+  coef_str[i] <- paste('\\multirow{2}{*}{$', clean_var[i], '$} & ', 
+                       put_stars(res[i,1], res[i,2], m, d), 
+                       '\\\\ \n & $(', format(round(res[i,3],d), scientific=F) ,
+                       ')$\\\\[3pt]\n', sep='')
+}
+cat(coef_str)
 
 
 
